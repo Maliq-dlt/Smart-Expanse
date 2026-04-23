fe964cabc50dd2b076119e573ea5874cb082f3cc
Maliq <maliq@example.com>
2026-04-24 06:29:12 +0700

    fix: resolve Lenis TypeScript and import errors

--- src/components/providers/SmoothScrollProvider.tsx
+++ src/components/providers/SmoothScrollProvider.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import { ReactNode, useEffect } from 'react';
-import Lenis from '@studio-freight/lenis';
+import Lenis from 'lenis';
 
 export default function SmoothScrollProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
       orientation: 'vertical',
       gestureOrientation: 'vertical',
       smoothWheel: true,
